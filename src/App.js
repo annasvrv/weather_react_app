71cbd643eae256c2da8965a34036483618bac1b8
Anna <anna.svrva@gmail.com>
2022-09-17 19:06:31 +0300

    favicon and GitHub source code link added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p className="mt-3">
+        <a
+          href="https://github.com/annasvrv/weather_react_app"
+          rel="noreferrer"
+          target="_blank"
+          title="GitHubCode"
+        >
+          Open-source code
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/anna-suvorova-1742b9197/"
+          target="_blank"
+          rel="noreferrer"
+          title="LinkedIn profile"
+        >
+          Anna S
+        </a>
+      </p>
     </div>
   );
 }
